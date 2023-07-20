3ba73e2d020c7b4c71c8678ea9efb4d3606733e5
asii123 <mohamedasik640@gmail.com>
2023-07-20 20:52:11 +0530

    asik code added

--- src/org/demo/Sample.java
+++ src/org/demo/Sample.java
@@ -1,9 +1,12 @@
 package org.demo;
 
 public class Sample {
+	
+	private void asik() {
+System.out.println("asik");
+	}
+	
 public static void main(String[] args) {
-	
-	
 	System.out.println("done");
 }
 }
