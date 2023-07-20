eafb9df59194ff9982caa769ce498e63e9ad0b9a
asii123 <mohamedasik640@gmail.com>
2023-07-20 21:04:45 +0530

    shuhul code added

--- src/org/demo/Sample.java
+++ src/org/demo/Sample.java
@@ -1,9 +1,12 @@
 package org.demo;
 
 public class Sample {
+	
+	private void shahul() {
+System.out.println("shahul");
+	}
+	
 public static void main(String[] args) {
-	
-	
 	System.out.println("done");
 }
 }
